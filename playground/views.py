62d76ef4b420c7a9f4331e2f5722363f2aee2d1c
issa2580 <issadiop2580@gmail.com>
2024-09-21 16:39:41 +0000

    improve app

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Customer
 
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    # customer = Customer.objects.filter(membership=Customer.MEMBERSHIP_BRONZE).order_by('first_name')[5:15]
+    customer = Customer.objects.values('first_name', 'last_name', 'membership').order_by('first_name')[5:15]
+    
+    
+    return render(request, 'hello.html', {'name': 'Mosh', 'customers': customer})
--- store/models.py
+++ store/models.py
@@ -32,7 +32,7 @@ class Customer(models.Model):
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
-    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    membership = models.CharField(max_length=10, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
     
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
